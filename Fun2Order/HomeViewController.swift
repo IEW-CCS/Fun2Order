1894a19b8e2680339730a38af97daee951c2742e
James <robohood83@gmail.comgit config --global user.name james>
2019-10-11 11:19:27 +0800

    20191011-1 Add animated banner in Home screen

--- Fun2Order/HomeViewController.swift
+++ Fun2Order/HomeViewController.swift
@@ -2,80 +2,103 @@
 //  HomeViewController.swift
 //  Fun2Order
 //
-//  Created by Lo Fang Chou on 2019/10/9.
+//  Created by Lo Fang Chou on 2019/10/10.
 //  Copyright © 2019 JStudio. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UITableViewController {
+class HomeViewController: UIViewController, FSPagerViewDataSource, FSPagerViewDelegate {
+    @IBOutlet weak var pageControl: FSPageControl!
+    
+    @IBOutlet weak var pagerView: FSPagerView!
+    
+    fileprivate let imageNames = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"]
+    fileprivate let transformerNames = ["cross fading", "zoom out", "depth", "linear", "overlap", "ferris wheel", "inverted ferris wheel", "coverflow", "cubic"]
+    fileprivate let transformerTypes: [FSPagerViewTransformerType] = [.crossFading,
+                                                              .zoomOut,
+                                                              .depth,
+                                                              .linear,
+                                                              .overlap,
+                                                              .ferrisWheel,
+                                                              .invertedFerrisWheel,
+                                                              .coverFlow,
+                                                              .cubic]
+    
+    fileprivate var typeIndex = 0 {
+        didSet {
+            let type = self.transformerTypes[typeIndex]
+            self.pagerView.transformer = FSPagerViewTransformer(type:type)
+            switch type {
+            case .crossFading, .zoomOut, .depth:
+                self.pagerView.itemSize = FSPagerView.automaticSize
+                self.pagerView.decelerationDistance = 1
+            case .linear, .overlap:
+                let transform = CGAffineTransform(scaleX: 0.6, y: 0.75)
+                self.pagerView.itemSize = self.pagerView.frame.size.applying(transform)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .ferrisWheel, .invertedFerrisWheel:
+                self.pagerView.itemSize = CGSize(width: 180, height: 140)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .coverFlow:
+                self.pagerView.itemSize = CGSize(width: 220, height: 170)
+                self.pagerView.decelerationDistance = FSPagerView.automaticDistance
+            case .cubic:
+                let transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.pagerView.itemSize = self.pagerView.frame.size.applying(transform)
+                self.pagerView.decelerationDistance = 1
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupPageView()
+    }
+    
+    private func setupPageView() {
+        self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "PageViewCell")
+        self.pagerView.automaticSlidingInterval = 3.0
+        self.pagerView.isInfinite = true
+        self.pagerView.delegate = self
+        self.pagerView.dataSource = self
+        self.pageControl.numberOfPages = self.imageNames.count
+        self.pageControl.contentHorizontalAlignment = .center
+        self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        self.typeIndex = 8
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let index = self.typeIndex
+        self.typeIndex = index // Manually trigger didSet
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    public func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return imageNames.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    
+    public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "PageViewCell", at: index)
+        cell.imageView?.image = UIImage(named: self.imageNames[index])
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.clipsToBounds = true
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
+        self.pageControl.currentPage = targetIndex
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
+        self.pageControl.currentPage = pagerView.currentIndex
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
